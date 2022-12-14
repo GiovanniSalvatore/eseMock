e9f93332cfccef0456d09030ee454e550c582a2c
Giovanni <giosal21@gmail.com>
2022-12-14 12:40:00 +0100

    eseMock-db

---
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -0,0 +1,18 @@
+package co.develhope.mockdb.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+}
